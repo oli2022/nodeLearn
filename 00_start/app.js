28364f02905366f09768ef9db44c356dc28b3fa7
Oli <oli0970717429@gmail.com>
2022-03-19 11:56:45 +0800

    feat:Node Learn

--- 00_start/app.js
+++ 00_start/app.js
@@ -1,7 +1,8 @@
 var http = require('http');
 http.createServer(function (request, response) {
+    console.log(request.url);
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('hello!!');
+    response.write('<h1>hello!!</h1>');
     response.end();
 }).listen(8080);
 //啟動server
