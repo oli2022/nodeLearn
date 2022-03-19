5f8faa030c51cf0b437ec9a7bf46151cf502d127
Oli <oli0970717429@gmail.com>
2022-03-19 20:35:14 +0800

    feat:調整表頭資訊，設置回傳 JSON 與 cors 資訊

--- 00_start/learn_06_todoList/server.js
+++ 00_start/learn_06_todoList/server.js
@@ -1,21 +1,32 @@
 const http = require('http');
 const requestListener = (req, res) => {
     const headers = {
-        'Content-type': 'text/plain',
+        //下面兩行跨網域使用的
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json',
     };
     console.log(req.url);
     console.log(req.method);
     if (req.url === '/' && req.method === 'GET') {
         res.writeHead(200, headers);
-        res.write('index');
-        res.end();
-    } else if (req.url === '/' && req.method === 'DELETE') {
-        res.writeHead(200, headers);
-        res.write('刪除成功');
+        res.write(
+            JSON.stringify({
+                //把json格式轉成字串傳出去
+                status: '成功',
+                data: [], //如果傳輸成功，要回傳什麼回去，如果沒有要回傳的資料就放個空陣列。
+            })
+        );
         res.end();
     } else {
         res.writeHead(404, headers);
-        res.write('not found 404');
+        res.write(
+            JSON.stringify({
+                status: '失敗',
+                message: '無此網站路由',
+            })
+        );
         res.end();
     }
 };
