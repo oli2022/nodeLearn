55b796a8351631322f3d794d95e73990c83397ed
Oli <oli0970717429@gmail.com>
2022-03-19 20:14:04 +0800

    feat:新增首頁測試網址與 404 頁面

--- 00_start/learn_06_todoList/server.js
+++ 00_start/learn_06_todoList/server.js
@@ -1,8 +1,23 @@
 const http = require('http');
 const requestListener = (req, res) => {
-    res.writeHead(200, { 'Content-type': 'text/plain' });
-    res.write('hello!!');
-    res.end();
+    const headers = {
+        'Content-type': 'text/plain',
+    };
+    console.log(req.url);
+    console.log(req.method);
+    if (req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, headers);
+        res.write('index');
+        res.end();
+    } else if (req.url === '/' && req.method === 'DELETE') {
+        res.writeHead(200, headers);
+        res.write('刪除成功');
+        res.end();
+    } else {
+        res.writeHead(404, headers);
+        res.write('not found 404');
+        res.end();
+    }
 };
 const server = http.createServer(requestListener);
 server.listen(3005);
